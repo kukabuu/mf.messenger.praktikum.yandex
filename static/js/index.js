660bc67e452ee43f7e014a3272e78216e14a9633
Ekaterina Blinova <eblinova@outlook.com>
2020-11-18 20:36:50 +0300

    Add static html

---
+++ static/js/index.js
@@ -0,0 +1,33 @@
+(function() {
+  collectFormData();
+  displayFileName();
+})()
+
+function collectFormData() {
+  const $form = document.querySelector('.js-form');
+  const formFields = {};
+  if ($form) {
+    $form.onsubmit = (e) => {
+      e.preventDefault();
+      const formData = new FormData($form);
+      for(let pair of formData.entries()) {
+        formFields[pair[0]] = pair[1];
+      }
+      console.log(formFields);
+    };
+  }
+}
+
+function displayFileName() {
+  const $fileInput = document.querySelector('.js-file-upload');
+  const $fileLabel = document.querySelector('.js-file-upload--label');
+  const $nameFile = document.querySelector('.js-file-name');
+  if ($fileInput) {
+    $fileInput.onchange = () => {
+      const name = $fileInput.files[0]?.name;
+      $nameFile.textContent = name;
+      $nameFile.classList.remove('i-display-none')
+      $fileLabel.classList.add('i-display-none');
+    }
+  }
+}
