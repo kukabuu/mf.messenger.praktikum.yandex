dfb3761716a3c32966f066ffa47ca024e5a56ef0
Ekaterina Blinova <eblinova@outlook.com>
2020-11-22 19:11:43 +0300

    Fix index.js after code review

--- static/js/index.js
+++ static/js/index.js
@@ -1,35 +1,35 @@
 (function() {
   collectFormData();
   displayFileName();
-})()
 
-function collectFormData() {
-  const $forms = document.querySelectorAll('.js-form');
-  if ($forms) {
-    [...$forms].forEach(($form) => {
-      $form.addEventListener('submit', (e) => {
-        const formFields = {};
-        e.preventDefault();
-        const formData = new FormData($form);
-        for (let pair of formData.entries()) {
-          formFields[pair[0]] = pair[1];
-        }
-        console.log(formFields);
+  function collectFormData() {
+    const $forms = document.querySelectorAll('.js-form');
+    if ($forms) {
+      [...$forms].forEach(($form) => {
+        $form.addEventListener('submit', (e) => {
+          const formFields = {};
+          e.preventDefault();
+          const formData = new FormData($form);
+          for (const pair of formData.entries()) {
+            formFields[pair[0]] = pair[1];
+          }
+          console.log(formFields);
+        });
       });
-    });
-  }
-}
-
-function displayFileName() {
-  const $fileInput = document.querySelector('.js-file-upload');
-  const $fileLabel = document.querySelector('.js-file-upload--label');
-  const $nameFile = document.querySelector('.js-file-name');
-  if ($fileInput) {
-    $fileInput.onchange = () => {
-      const name = $fileInput.files[0]?.name;
-      $nameFile.textContent = name;
-      $nameFile.classList.remove('i-display-none')
-      $fileLabel.classList.add('i-display-none');
     }
   }
-}
+  
+  function displayFileName() {
+    const $input = document.querySelector('.js-file-upload');
+    const $label = document.querySelector('.js-file-upload--label');
+    const $name = document.querySelector('.js-file-name');
+    if ($input) {
+      $input.onchange = () => {
+        const name = $input.files[0]?.name;
+        $name.textContent = name;
+        $name.classList.remove('i-display-none')
+        $label.classList.add('i-display-none');
+      }
+    }
+  }
+})()
